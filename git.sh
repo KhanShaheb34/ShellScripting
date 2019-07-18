999570372171c616226c1f3345ba7c468e6030a9
Shakirul Hasan <shakirul34@student.sust.edu>
2019-07-18 16:22:51 +0600

    First commit using git.sh

--- git.sh
+++ git.sh
@@ -7,8 +7,6 @@ echo "Adding everything..."
 git add .
 echo "Commiting \"$COMMIT_MESSAGE\""...
 git commit -m "$COMMIT_MESSAGE"
-echo "Pushing using username KhanShaheb34..."
-echo -n "Password for KhanShaheb34: "
-read -s PASS
+echo "Pushing..."
 git push origin master
 echo "Succesfully Pushed!"
