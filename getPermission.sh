b0ff462af65c428473ab4767cc7fc7dd7efa6991
Shakirul Hasan <shakirul34@student.sust.edu>
2019-07-18 13:22:51 +0600

    First commit using git helper

---
+++ getPermission.sh
@@ -0,0 +1,3 @@
+echo "Taking permission to run every sh file from this directory..."
+chmod +x *.sh
+echo "Permission Granted!"
---
+++ git.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+git status
+echo -n "Commit Message: "
+read COMMIT_MESSAGE
+echo "Adding everything..."
+git add .
+echo "Commiting \"$COMMIT_MESSAGE\""...
+git commit -m "$COMMIT_MESSAGE"
+echo "Pushing using username KhanShaheb34..."
+echo -n "Password for KhanShaheb34: "
+read -s PASS
+git push origin master
+KhanShaheb34
+$PASS
+echo "Succesfully Pushed!"
